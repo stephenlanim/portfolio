123873d13502c52a8e962a640b3b8ad9461b033f
Stephen Anim <stephen.lloyd.anim@gmail.com>
2018-09-16 19:29:47 -0400

    Began switching out project-info and project-summary.
    I realized that the items I had in project-info were more at-a-glance and should really be the stuff on the project card, while the project-summary content required more reading and should be in the modal.
    I also wrote notes to refactor the accordion code, since I will soon need multiple accordions in the same container.

--- js/accordions.js
+++ js/accordions.js
@@ -8,17 +8,22 @@ These scripts can be used to turn an HTML object into a trigger for an accordion
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
+// 09-16-2018: I'm gonna need to redo how the accordions work. Right now they work using DOM traversal on classes, but that only works well if there is only one trigger-content pair within a container. With multiple content objects in the same container, a given trigger in that container will open ALL the content objects.
+// So here are my options:
+// 1) Match the trigger and content by ID, which will require turning all triggers into <a> tags with hrefs and giving an ID to every content object.
+// 2) Loop through the triggers and add an event listener that oly activates on the content object that has the same index number. This will require less HTML markup going forward, and I can mimic the code I used in the Dashboard App project. I may still use classes to match up the TYPES of triggers and contents.
 
-function btnAccordion(trigger, content) {
+function btnAccordion(triggerClass, contentClass) {
+
   // Initially hide accordion content
-  $(`${content}`).hide();
+  $(`${contentClass}`).hide();
 
   // When trigger is clicked...
-  $(`${trigger}`).click(function(e){
+  $(`${triggerClass}`).click(function(e){
     const $clickedTrigger = $(e.target);
 
     // show accordion content if hidden, close if shown
-    $clickedTrigger.parent().find(`${content}`).toggle(200);
+    $clickedTrigger.parent().find(`${contentClass}`).toggle(200);
     // rotate arrow in trigger
     $clickedTrigger.parent().find('i').toggleClass('rotateCCW90');
 
